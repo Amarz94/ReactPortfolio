fc967e9188fea43b137521ec256fbf97ee0f065d
Alex Marzullo <marzullo94@gmail.com>
2021-04-20 14:54:08 -0400

    Updates

---
+++ precache-manifest.e6e035b15559bee4ce99369690f826e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b84413b8637954d61fc",
+    "url": "/reactportfolio/static/css/main.52e79d1e.chunk.css"
+  },
+  {
+    "revision": "3b84413b8637954d61fc",
+    "url": "/reactportfolio/static/js/main.3b84413b.chunk.js"
+  },
+  {
+    "revision": "3b18ef2a865ad29f2312",
+    "url": "/reactportfolio/static/js/1.3b18ef2a.chunk.js"
+  },
+  {
+    "revision": "f3444e3a511a2aa26237",
+    "url": "/reactportfolio/static/js/runtime~main.f3444e3a.js"
+  },
+  {
+    "revision": "abb2c0beef471f85bdbaaa301f096cb1",
+    "url": "/reactportfolio/static/media/resume.abb2c0be.pdf"
+  },
+  {
+    "revision": "8bf9fa7574cd34ed79ec61fdc025485c",
+    "url": "/reactportfolio/static/media/gitresume.8bf9fa75.JPG"
+  },
+  {
+    "revision": "124e23be0f65425d2fcd67f9d82a6e35",
+    "url": "/reactportfolio/static/media/whoiswho.124e23be.JPG"
+  },
+  {
+    "revision": "754ad8dee67b0ba9d1d7596be8649edd",
+    "url": "/reactportfolio/static/media/gametag.754ad8de.JPG"
+  },
+  {
+    "revision": "6930ee20e1bb749ec855e337d2092e85",
+    "url": "/reactportfolio/static/media/fitness.6930ee20.jpg"
+  },
+  {
+    "revision": "0c1e1d102f9892c11aab869f6fb25de7",
+    "url": "/reactportfolio/index.html"
+  }
+];
